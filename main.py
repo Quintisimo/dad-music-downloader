711b52b145c7f97c3b0de3d844f16e6fdafbdce1
Quintus Cardozo <quintuscardozo13@gmail.com>
2026-06-21 00:12:23 +1000

    feat: queue multiple songs for download

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ import cutie
 import yt_dlp
 from youtube_search import YoutubeSearch
 
-try:
-    search = input("Search for a song: ")
-    results = YoutubeSearch(search, max_results=10).to_dict()
+def clear_terminal():
+    print("\033[H\033[J", end="")
 
-    titles = [
-        f"{result['title']} [{result['channel']}] [{result['duration']}] [{result['views']}]"
-        for result in results
-    ]
-    video = results[cutie.select(titles)]
-    name = video["title"]
-    url = f"https://www.youtube.com/watch?v={video['id']}"
+try:
+    video_list = []
+    add_song = True
+
+    while add_song:
+      search = input("Search for a song: ")
+      results = YoutubeSearch(search, max_results=10).to_dict()
+      titles = [
+          f"{result['title']} [{result['channel']}] [{result['duration']}] [{result['views']}]"
+          for result in results
+      ]
+      video = results[cutie.select(titles)]
+      video_list.append(video)
+      add_song = cutie.prompt_yes_or_no("Add another song?")
+      clear_terminal()
+      print(f"Songs to be downloaded: {[video['title'] for video in video_list]}\n")
 except KeyboardInterrupt:
     exit(0)
 
@@ -24,53 +32,63 @@ except KeyboardInterrupt:
 # https://www.xmodhub.com/info/xmod-blog/dead-as-disco-custom-music/
 sample_rate = "44100"
 format = "ogg"
-folder = f"songs/{name}"
-song_path = f"{folder}/Audio"
 
-ydl_opts = {
-    "format": f"{format}/bestaudio/best",
-    "outtmpl": song_path,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "vorbis",
-            "preferredquality": "192",  # bitrate in kbps (optional)
-        }
-    ],
-    "postprocessor_args": {
-        "extractaudio": ["-ar", sample_rate],  # sample rate → ffmpeg
-    },
-}
+for video in video_list:
+  name = video["title"]
+  url = f"https://www.youtube.com/watch?v={video['id']}"
+  folder = f"songs/{name}"
+  song_path = f"{folder}/Audio"
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    error_code = ydl.download([url])
-    if error_code:
-        print(f"Download failed with error code: {error_code}")
-        exit(1)
+  clear_terminal()
+  print(f"Downloading {name}...")
 
-    bpm = subprocess.check_output(
-        # https://gist.github.com/brimston3/34dbb439442a723313b019b92931887b
-        f'ffmpeg -vn -i "{song_path}.{format}" -ar {sample_rate} -ac 1 -f f32le pipe:1 2>/dev/null | bpm',
-        shell=True,
-        text=True,
-    ).strip()
+  ydl_opts = {
+      "quiet": True,
+      "format": f"{format}/bestaudio/best",
+      "outtmpl": song_path,
+      "postprocessors": [
+          {
+              "key": "FFmpegExtractAudio",
+              "preferredcodec": "vorbis",
+              "preferredquality": "192",  # bitrate in kbps (optional)
+          }
+      ],
+      "postprocessor_args": {
+          "extractaudio": ["-ar", sample_rate],  # sample rate → ffmpeg
+      },
+  }
 
-    meta = {
-      "version": 1,
-      "uniqueId": random.randint(100000000, 999999999),
-      "songName": name,
-      "performedBy": [video["channel"]],
-      "writtenBy": [video["channel"]],
-      "seed": random.randint(100000000, 999999999),
-      "tempo": int(float(bpm)),
-      "customTempoSections": [],
-      "beatOffset": 0,
-      "startSongOffset": 0,
-      "endSongOffset": 0,
-      "uEAssetName": name,
-      "originalAudioFileHash": "",
-      "originalAudioFilePath": ""
-    }
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      error_code = ydl.download([url])
+      if error_code:
+          print(f"Download failed with error code: {error_code}")
+          exit(1)
 
-    with open(f"{folder}/Meta.json", "w") as f:
-        json.dump(meta, f, indent=2)
+      bpm = subprocess.check_output(
+          # https://gist.github.com/brimston3/34dbb439442a723313b019b92931887b
+          f'ffmpeg -vn -i "{song_path}.{format}" -ar {sample_rate} -ac 1 -f f32le pipe:1 2>/dev/null | bpm',
+          shell=True,
+          text=True,
+      ).strip()
+
+      meta = {
+        "version": 1,
+        "uniqueId": random.randint(100000000, 999999999),
+        "songName": name,
+        "performedBy": [video["channel"]],
+        "writtenBy": [video["channel"]],
+        "seed": random.randint(100000000, 999999999),
+        "tempo": int(float(bpm)),
+        "customTempoSections": [],
+        "beatOffset": 0,
+        "startSongOffset": 0,
+        "endSongOffset": 0,
+        "uEAssetName": name,
+        "originalAudioFileHash": "",
+        "originalAudioFilePath": ""
+      }
+
+      with open(f"{folder}/Meta.json", "w") as f:
+          json.dump(meta, f, indent=2)
+
+print("All songs downloaded successfully!")
